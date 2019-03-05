a7b177088d80cdd74db5407a9bbb8918df95e89d
Alexander Gladun <sashko@my-server.km.ua>
2019-03-05 11:38:33 +0200

    Add utils and sleep

---
+++ utils.cpp
@@ -0,0 +1,19 @@
+
+#include <stdlib.h>
+#include <string.h>
+#include "utils.h"
+
+
+char *strcopy(const char *source){
+  char *res;
+  size_t str_size = strlen(source);
+  res = (char*) malloc((str_size + 1) * sizeof(char));
+  strncpy(res, source, (str_size + 1) * sizeof(char));
+  return res;
+}
+
+char *strinit(const unsigned int str_size){
+  char *res;
+  res = (char*) malloc((size_t) (str_size + 1) * sizeof(char));
+  return res;
+}
---
+++ utils.h
@@ -0,0 +1,8 @@
+
+#ifndef UTILS_H_INCLUDED
+#define UTILS_H_INCLUDED
+
+char *strcopy(const char *source);
+char *strinit(const unsigned int str_size);
+
+#endif // UTILS_H_INCLUDED
